58a5c5f5cba4e985fd72c6463623730a8f768b75
Harsh Kumar Sinha <hksinha960@gmail.com>
2024-07-02 17:16:26 +0530

    Firebase issue 3

--- config/firebase.js
+++ config/firebase.js
@@ -2,15 +2,15 @@ import admin from "firebase-admin";
 
 import dotenv from "dotenv";
 dotenv.config();
-console.log(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON));
 try {
+  console.log(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON));
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
   console.log("Firebase initialized successfully");
 } catch (error) {
-  console.error("Error parsing FIREBASE_SERVICE_ACCOUNT_JSON:", error.message);
+  console.error(error);
 }
 
 export default admin;
